962ecdef377ba24bad55ff651cffa4b380477331
tarajeanne <t.jeanne.patel@gmail.com>
2019-12-01 21:33:02 -0600

    experimenting with print vs println

---
+++ com/semanticsquare/basics/demo.java
@@ -0,0 +1,9 @@
+class Demo {
+  int id1 = 0;
+  
+  public static void main(String[] args) {
+    System.out.println("\n\nInside Print");
+    System.out.print("this is pring");
+    System.out.println("this should be on the samelibne?");
+  }
+}
