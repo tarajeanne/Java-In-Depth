b7204ce9c558fc85a2017975f875e9b932e47386
tarajeanne <t.jeanne.patel@gmail.com>
2019-12-01 20:38:10 -0600

    added demo file

--- com/semanticsquare/basics/HelloWorld.java
+++ com/semanticsquare/basics/HelloWorld.java
@@ -2,10 +2,4 @@ public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello, world!");
   }
-}
-
-class Goodbye {
-  public static void main(String args[]) {
-    System.out.println("Goodbye, world!");
-  }
-}
+}  
